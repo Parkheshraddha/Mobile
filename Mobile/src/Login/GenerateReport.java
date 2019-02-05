120e9c35951764332d4769c6d71195b9bc59894f
parkhe <parkhe@SHRADDHA-PC>
2019-02-05 16:13:20 +0530

    My whole project

---
+++ Mobile/src/Login/GenerateReport.java
@@ -0,0 +1,11 @@
+package Login;
+
+import org.automationtesting.excelreport.Xl;
+
+public class GenerateReport {
+	public static void main(String[]args) throws Exception
+	{
+		Xl.generateReport("D:\\","excel1-report.xlsx");
+	}
+
+}
